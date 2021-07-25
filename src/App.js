c9f6b218d68defddb8ef080b90f436ffe0aa9271
Iryna Volosozharova <iradevis@gmail.com>
2021-07-25 17:46:16 +0300

    Added start files of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Component } from "react";
+import Button from "./components/Button";
+import ImageGallery from "./components/ImageGallery";
+import ImageGalleryItem from "./components/ImageGalleryItem";
+import Loader from "./components/Loader";
+import Modal from "./components/Modal";
+import Searchbar from "./components/Searchbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "modern-normalize";
+import "./styles.css";
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
